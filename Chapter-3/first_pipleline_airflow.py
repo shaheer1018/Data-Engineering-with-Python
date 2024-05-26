4ba9a21f3a8d2a4fc38f1ec06e1bdaab56b4c6d6
Shaheer <shaheer301910@gmail.com>
2024-05-26 22:25:02 +0500

    first_pipeline_airflow (path error fixed)

--- Chapter-3/first_pipleline_airflow.py
+++ Chapter-3/first_pipleline_airflow.py
@@ -7,19 +7,18 @@ from airflow.operators.python_operator import PythonOperator
 import pandas as pd
 
 
-PATH_DATA = r'C:\Users\shahe\Jupyter Notebooks\Data Engineering\Data-Engineering-with-Python\Chapter-3'
+PATH_DATA = '/opt/airflow/data'
 
 def CSVToJson():
-    df = pd.read_CSV(os.path.join(PATH_DATA, 'data.CSV'))
+    df = pd.read_csv(os.path.join(PATH_DATA, 'data.CSV'))
     for i, r in df.iterrows():
         print(r['name'])
+    df.to_json('fromAirflow.JSON', orient = 'records')
 
-default_args = {
-    'owner': 'shaheer',
-    'start_date': dt.datetime(2024, 5, 25),
-    'retries': 1,
-    'retry_delay': dt.timedelta(minutes=5),
-}
+default_args = {'owner': 'shaheer',
+                'start_date': dt.datetime(2024, 5, 25),
+                'retries': 1,
+                'retry_delay': dt.timedelta(minutes=5)}
 
 with DAG('MyCSVDAG',
          default_args=default_args,
@@ -28,7 +27,9 @@ with DAG('MyCSVDAG',
          ) as dag:
      
      print_starting = BashOperator(task_id='starting', bash_command='echo "I am reading the CSV now....."')
+    
      CSVJson = PythonOperator(task_id='convertCSVtoJson', python_callable=CSVToJson)
+
 
 # downstream
 print_starting >>  CSVJson
